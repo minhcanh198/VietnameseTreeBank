11740184ac5821c38ac54a4a391c90bc24954fbb
Trong <tranthientrong1998@gmail.com>
2019-12-30 16:07:09 +0700

    fix output beauty

---
+++ sort.py
@@ -0,0 +1,17 @@
+f = open("output.txt", encoding="utf-8")
+# print(f.read())
+
+array=''
+array_2=''
+array_3=''
+for i in f:
+    # print(f[i])
+    result = i.find("-> '")
+    if(result == -1):
+        # array.append[i]
+        array=array+i
+    else:
+        array_2=array_2+i
+
+array_3=array+array_2
+print(array_3)
